5038849ba9f36c4252ba225bca593730ef339a53
Fabien Romano <fabienromano@gmail.com>
2025-10-19 16:54:28 +0100

    update GitHub handler, fix a few re, catch git url and use extractgit

--- Portroach/SiteHandler/GitHub.pm
+++ Portroach/SiteHandler/GitHub.pm
@@ -1,5 +1,6 @@
 #------------------------------------------------------------------------------
 # Copyright (C) 2014, 2020 Jasper Lievisse Adriaanse <jasper@openbsd.org>
+# Copyright (C) 2025 Fabien Romano <fabien@openbsd.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -30,7 +31,6 @@ use strict;
 
 require 5.006;
 
-
 #------------------------------------------------------------------------------
 # Globals
 #------------------------------------------------------------------------------
@@ -39,7 +39,7 @@ push @Portroach::SiteHandler::sitehandlers, __PACKAGE__;
 
 our %settings;
 
-my $github_re = qr/https?\:\/\/github\.com/;
+my $github_re = qr/(?:https?|git)\:\/\/github\.com/;
 
 #------------------------------------------------------------------------------
 # Func: new()
@@ -87,18 +87,17 @@ sub CanHandle
 #
 # Args: $url - A URL we want to extract name from.
 #
-# Retn: undef
+# Retn: $name or undef
 #------------------------------------------------------------------------------
 
 sub GetName
 {
 	my $self = shift;
-
 	my ($url) = @_;
 
 	if ($url =~ /^$github_re\/downloads\/(.*)\//) {
 		return $1;
-	} elsif ($url =~ /^$github_re\/([^\/]+)\/([^\/]+)\//) {
+	} elsif ($url =~ /^$github_re\/([^\/]+)\/([^\/]+)(\/|$)/) {
 		return "$1/$2";
 	} else {
 		return undef;
@@ -112,34 +111,25 @@ sub GetName
 #
 # Args: $url - A URL we want to extract name from.
 #
-# Retn: undef
+# Retn: $ver or undef
 #------------------------------------------------------------------------------
 
 sub GetVersion
 {
 	my $self = shift;
-
 	my ($ver) = @_;
-
-	my $projname = $self->GetName($ver);
-	return undef unless ($projname);
-
-	debug(__PACKAGE__, undef, "find $projname");
-	my ($account, $repo) = split('/', $projname);
 
 	return undef if (
 	    $ver !~ s:^$github_re/.*?/archive/refs/tags/(release/)?(.*)/.*?$:$2:
 	    && $ver !~ s:^$github_re/.*?/releases/download/(.*?)/.*$:$1:);
-	$ver =~ s/^($account|$repo)[\-\_]?//g;
-	$ver =~ s/\//\+/g;
 	return $ver;
 }
 
+
 #------------------------------------------------------------------------------
 # Func: GetFiles()
-# Desc: Extract a list of files from the given URL. In the case of GitHub,
-#       we are actually pulling the files from the project's Atom feed and
-#       extract the release url, containing the tag it was based on.
+# Desc: Extract a list of files from the given URL. Check for latest release
+#       and pull tags from git command.
 #
 # Args: $url     - URL we would normally fetch from.
 #       \%port   - Port hash fetched from database.
@@ -153,9 +143,8 @@ sub GetFiles
 	my $self = shift;
 
 	my ($url, $port, $files) = @_;
-	my (@tags, $projname, $query, $ua, $req, $resp, $json, $ver);
+	my ($projname, $query, $ua, $req, $resp, $json);
 
-	# XXX check if $projname =~ /.*?\/(.*)/ ?
 	$projname = $self->GetName($url);
 	unless ($projname) {
 		print STDERR "$port->{fullpkgpath}: $url, "
@@ -185,18 +174,12 @@ sub GetFiles
 		# XXX maybe drop this: what matter is version detection thus
 		#     we don't care if the new url isn't pointing to an asset
 		foreach my $asset (@{$json->{assets}}) {
-			if ($asset->{name} !~ m/$port->{sufx}$/) {
-				debug(__PACKAGE__, $port,
-				    "$asset->{name} !~ $port->{sufx}");
-				next;
-			}
 			push(@$files, $asset->{browser_download_url});
 		}
 
-		# Anyway push the tag_name as version
 		# XXX tmp fix, in case tags got messy and older pop first
-		push(@tags, $json->{tag_name});
-
+		# Anyway push the tag_name as version
+		# push(@tags, $json->{tag_name});
 	} else {
 		if ($resp->header('x-ratelimit-remaining') == 0) {
 			print STDERR "$port->{fullpkgpath}: "
@@ -208,62 +191,10 @@ sub GetFiles
 		    . ': ' . $resp->status_line);
 	}
 
-	# Project may not do releases (any more), so let's load tags anyway.
-	# API /tags are sorted based on git output (alphabetical) and returns
-	# only top X tags. GraphQL looks harder to use. Then, use git directly.
-	my $gitcmd = "GIT_TERMINAL_PROMPT=0 /usr/local/bin/git".
-	    " -c 'versionsort.suffix=-' ls-remote".
-	    " --tags --sort='v:refname'";
-	my $giturl = "https://github.com/$projname.git";
-	debug(__PACKAGE__, $port, "$gitcmd $giturl");
-	my ($out, $err, $rc) = capture {
-		system("$gitcmd $giturl");
-	};
-	if ($rc != 0 or $err) {
-		$err .= '\n' if ($err !~ /\n$/g);
-		info(1, $port->{fullpkgpath}, strchop($giturl, 60)
-		    . ': ' . "$rc, $err");
-		return 0;
-	}
-	foreach my $output (reverse(split /^/m, $out)) {
-		my ($commit, $tag) = split(/\s+/, $output);
-		next if ($tag =~ /\^\{\}$/);
-		$tag =~ s/refs\/tags\///;
-		debug(__PACKAGE__, $port, "$tag");
-		push(@tags, $tag);
-	}
-
-	# In some cases the project name (read: repo) is part of the tagname.
-	# For example: 'heimdal-7.3.0' is the full tagname. Therefore remove the
-	# repository name from the filename just in case.
-	my ($account, $repo) = split('/', lc $projname);
+	# Extract versions from git tags
+	$query = "https://github.com/$projname.git";
 	$url = "https://github.com/$projname/archive/refs/tags/";
-
-	foreach my $tag (@tags) {
-		my $ver = lc $tag;
-		$tag =~ s/\//%2F/;
-
-		debug(__PACKAGE__, $port, "replace / by _ -> $ver")
-		    if ($ver =~ s/\//\_/g);
-		debug(__PACKAGE__, $port, "trim projname -> $ver")
-		    if ($ver =~ s/($account|$repo)[\-\_]?//);
-		debug(__PACKAGE__, $port, "trim (v|r) marker -> $ver")
-		    if ($ver =~ s/^$verprfx_regex//);
-		debug(__PACKAGE__, $port, "trim \\D SEP -> $ver")
-		    if ($ver =~ s/^\D*[\.\-\_]//);
-
-		debug(__PACKAGE__, $port, "tag $tag -> $ver")
-		    if ($tag ne $ver);
-		debug(__PACKAGE__, $port, "stop loading tag, old found $ver")
-		    if ($ver eq $port->{ver});
-
-		$ver = "$url$tag$port->{sufx}%%$ver";
-		push(@$files, $ver);
-
-		# XXX tmp fix, need graphql to order tag correclty
-		$tag = quotemeta $port->{ver};
-		last if ($ver =~ /$tag/);
-	}
+	return 0 if (extractgit($port, $query, $url, $files) < 0);
 
 	return 1;
 }
